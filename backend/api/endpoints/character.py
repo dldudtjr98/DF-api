a15c590a1abe11fd2e73747ca7203a9157e87d52
dldudtjr98 <dldudtjr98@gmail.com>
2021-06-08 18:58:04 +0900

    add skill critical calculate

--- backend/api/endpoints/character.py
+++ backend/api/endpoints/character.py
@@ -94,6 +94,39 @@ async def search_skill_enforce_info(server, char_id, category):
     return data
 
 
+def get_character_critical_skill(baseId, advancedId):
+    for skill in settings.CRIT_SKILL_ACTIVE:
+        if skill["jobId"]["baseId"] == baseId and skill["jobId"]["advancedId"] == advancedId:
+            return skill["skillId"]
+    return None
+
+
+def calc_skill_critical(crit_skill, skill_id, buff_skill_lv):
+    initial_lv_crit = crit_skill[skill_id][0]
+    increase_per_lv_crit = crit_skill[skill_id][1]
+    add_critical = initial_lv_crit + increase_per_lv_crit * (buff_skill_lv - 1)
+    return add_critical
+
+
+def calc_add_critical(crit_skill, buff_skill, skill_data):
+    buff_skill_critical = 0
+    normal_skill_active_critical = 0
+    normal_skill_passive_critical = 0
+    if crit_skill:
+        for skill_id in crit_skill.keys():
+            if buff_skill["skillId"] == skill_id:
+                buff_skill_critical = calc_skill_critical(crit_skill, skill_id, buff_skill["option"]["level"])
+            else:
+                for active in skill_data["skill"]["style"]["active"]:
+                    if active["skillId"] == skill_id:
+                        normal_skill_active_critical = calc_skill_critical(crit_skill, skill_id, active["level"])
+                for passive in skill_data["skill"]["style"]["passive"]:
+                    if passive["skillId"] == skill_id:
+                        normal_skill_passive_critical = calc_skill_critical(crit_skill, skill_id, active["level"])
+    add_critical = buff_skill_critical + normal_skill_active_critical + normal_skill_passive_critical
+    return add_critical
+
+
 async def get_crit(request):
     status_data = await search_status_info(request.serverId, request.characterId)
     try:
@@ -122,6 +155,11 @@ async def search_detail_user(request: SearchDetail):
     skill_data = await search_skill_info(request.serverId, request.characterId)
     skill_enforce_equip_data = await search_skill_enforce_info(request.serverId, request.characterId, 'equipment')
     critical = await get_crit(request)
+
+    buff_skill = skill_enforce_equip_data["skill"]["buff"]["skillInfo"]
+    crit_skill = get_character_critical_skill(basic_data["jobId"], basic_data["jobGrowId"])
+    add_critical = calc_add_critical(crit_skill, buff_skill, skill_data)
+    critical += add_critical
 
     detail_skill_data = {}
     for active in skill_data["skill"]["style"]["active"]:
