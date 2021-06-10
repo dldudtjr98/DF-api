7955af9fe5fe5d71eecfc8f748c630640ceb6b3a
dldudtjr98 <dldudtjr98@gmail.com>
2021-06-10 15:07:16 +0900

    character search schemas

--- backend/api/models/character/schemas.py
+++ backend/api/models/character/schemas.py
@@ -1,8 +1,47 @@
 import datetime
 from typing import Optional
+from bson import ObjectId
+from pydantic.fields import Field
 
 from pydantic import BaseModel
 
+from modules import PyObjectId
 
-class CharacterBase(BaseModel):
-    last_update: datetime.datetime
+
+class CharacterBasicModel(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    characterId: str = Field(...)
+    characterName: str = Field(...)
+    level: int = Field(...)
+    jobId: str = Field(...)
+    jobGrowId: str = Field(...)
+    jobName: str = Field(...)
+    jobGrowName: str = Field(...)
+    adventureName: str = Field(...)
+    guildId: str = Field(...)
+    guildName: str = Field(...)
+
+    class Config:
+        json_encoders = {ObjectId: str}
+
+
+class CharacterModel(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    characterBasic: CharacterBasicModel = Field(...)
+    critical: float = Field(...)
+    regTime: datetime.datetime = Field(...)
+
+    class Config:
+        json_encoders = {ObjectId: str}
+
+
+class Search(BaseModel):
+    name: str
+    server: Optional[str] = 'all'
+    jobId: Optional[str] = ''
+    jobGrowId: Optional[str] = ''
+
+
+class SearchDetail(BaseModel):
+    serverId: str
+    characterId: str
